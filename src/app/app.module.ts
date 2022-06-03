1f1df37d82ea81464d2791415604755709d67cdb
Pablo Jarrin <pjarrin@sigma-alimentos.com>
2022-06-03 17:02:31 -0500

    Componentes por Pais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { PaisModule } from './pais/pais.module';
+import { AppRoutingModule } from './app-routing.module';
 
 
 
@@ -16,6 +17,7 @@ import { PaisModule } from './pais/pais.module';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     SharedModule,
     PaisModule
   ],
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorPaisComponent implements OnInit {
 
+  termino: string = '';
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  buscar(){
+    console.log(this.termino)
+  }
 }
